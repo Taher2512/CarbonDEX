fa0ffd4e08f26c59eae383ddf20574b8d9e4508d
Tarif-dev <tarifhussain212@gmail.com>
2024-06-10 21:36:41 +0530

    added subscribe page

---
+++ Components/Subcard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Subcard() {
+  return (
+    <div></div>
+  )
+}
+
+export default Subcard
---
+++ src/app/subscribe/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Subcard from '../../../Components/Subcard.jsx'
+
+function page() {
+  return (
+    <>
+   <Subcard/>
+    </>
+  )
+}
+
+export default page
