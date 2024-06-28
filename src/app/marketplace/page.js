1f7a21f3cecbf3fada795e4a21911d4506f43ec6
Tarif-dev <tarifhussain212@gmail.com>
2024-06-28 10:53:40 +0530

    connect wallet added to homepage

--- src/app/marketplace/page.js
+++ src/app/marketplace/page.js
@@ -4,7 +4,7 @@ import React from "react";
 import { ThirdwebProvider, metamaskWallet } from "@thirdweb-dev/react";
 import MarketplacePage from "../../../Components/MarketplacePage";
 
-const CarbonCreditListingPage = () => {
+const MarketPlace = () => {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
@@ -18,4 +18,4 @@ const CarbonCreditListingPage = () => {
   );
 };
 
-export default CarbonCreditListingPage;
+export default MarketPlace;
